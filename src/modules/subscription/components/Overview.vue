df4073a72d96b98bd44e6a7184ed9b13764bcbd9
Valentin Seehausen <valentin.seehausen@gmail.com>
2019-09-12 18:20:56 +0200

    add links to overview

--- src/modules/subscription/components/Overview.vue
+++ src/modules/subscription/components/Overview.vue
@@ -2,6 +2,8 @@
   <div>
     <h2>Subscription loaded:</h2>
     <div class="subscription" v-if="subscription">{{ subscription }}</div>
+    <router-link to="/subscription/products">{{ $t("Order Cacao Subscription") }}</router-link>
+    <router-link to="/subscription/products">{{ $t("My Subscription") }}</router-link>
   </div>
 </template>
 
